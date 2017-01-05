676374cb5f2ddf7d037320df9eb25b0a02f31793
NOrdenes <nicolas.ordenes@alumnos.usm.cl>
2017-01-05 16:00:46 -0300

    Forced failure

--- dot.py
+++ dot.py
@@ -8,3 +8,13 @@ def test_dot_zeros():
 	a = [0,0]
 	b = [0,0]
 	assert dot(a,b) == 0
+
+def test_dot_perp():
+	a = [0,1]
+	b = [1,0]
+	assert dot(a,b) == 0
+
+def test_diff_len():
+	a = [1,2,3]
+	b = [1,1]
+	assert dot(a,b) == 3
